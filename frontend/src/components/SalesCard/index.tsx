6433638912b43f82fd9a36a2312c2cbe2478af90
Marcio Ludwig <TI@NB-17.dipaes.local>
2022-07-17 20:29:57 -0300

    Axios, useEffect first request

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -2,33 +2,43 @@ import NotificationButtom from '../NotificationButton';
 import './styles.css';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 function SalesCard() {
 
     const min = new Date(new Date().setDate(new Date().getDate() - 365));
+    const max = new Date();
+
     const [minDate, setMinDate] = useState(min);
-    const [maxDate, setMaxDate] = useState(new Date());
+    const [maxDate, setMaxDate] = useState(max);
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/sales")
+        .then(response =>{
+            console.log(response.data);
+        })
+    }, []);
 
     return (<>
         <div className="dsmeta-card">
             <h2 className="dsmeta-sales-title">Vendas</h2>
             <div>
-                <div className="dsmeta-form-control-container">
-                    <td className="show576"><DatePicker
-                        selected={minDate}
-                        onChange={(date: Date) => { setMinDate(date) }}
-                        className="dsmeta-form-control"
-                        dateFormat="dd/MM/yyyy"
-                    /></td>
-                </div>
-                <div className="dsmeta-form-control-container">
-                    <td className="show576"><DatePicker
-                        selected={maxDate}
-                        onChange={(date: Date) => { setMaxDate(date) }}
-                        className="dsmeta-form-control"
-                        dateFormat="dd/MM/yyyy"
-                    /></td>
-                </div>
+                    <div className="dsmeta-form-control-container">
+                        <td className="show576"><DatePicker
+                            selected={minDate}
+                            onChange={(date: Date) => { setMinDate(date) }}
+                            className="dsmeta-form-control"
+                            dateFormat="dd/MM/yyyy"
+                        /></td>
+                    </div>
+                    <div className="dsmeta-form-control-container">
+                        <td className="show576"><DatePicker
+                            selected={maxDate}
+                            onChange={(date: Date) => { setMaxDate(date) }}
+                            className="dsmeta-form-control"
+                            dateFormat="dd/MM/yyyy"
+                        /></td>
+                    </div>
             </div>
             <div>
                 <table className="dsmeta-sales-table">
